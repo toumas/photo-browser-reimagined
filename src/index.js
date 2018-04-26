6a32504a14e01309d7d01299469e3021bf01da23
toumas <tuomasukk@gmail.com>
2018-04-26 23:36:14 +0300

    Added redux example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import Home from './Home';
 
-const App = () => <h1>Hi there</h1>;
+const store = createStore(
+  reducers,
+  // eslint-disable-next-line no-underscore-dangle, no-undef
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+const App = () => (
+  <Provider store={store}>
+    <Home />
+  </Provider>
+);
 
 // eslint-disable-next-line no-undef
 render(<App />, document.getElementById('app'));
