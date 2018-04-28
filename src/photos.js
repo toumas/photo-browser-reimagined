42aad79f38f82979e6a768b71cc2d4d22cfee87e
toumas <tuomasukk@gmail.com>
2018-04-28 16:56:33 +0300

    Implemented api connection, photos duck, PhotosContainer & PhotoList

---
+++ src/photos.js
@@ -0,0 +1,40 @@
+import { normalize } from 'normalizr';
+import { getPhotos } from './api';
+import { photoList } from './schemas';
+
+const LOADING = 'APP/PHOTOS/LOADING';
+const SUCCESS = 'APP/PHOTOS/SUCCESS';
+const FAIL = 'APP/PHOTOS/FAIL';
+
+export default function reducer(
+  state = { failed: false, isLoading: false, items: {} },
+  action = {},
+) {
+  switch (action.type) {
+    case LOADING:
+      return { ...state, failed: false, isLoading: action.isLoading };
+    case SUCCESS:
+      return { ...state, isLoading: action.isLoading, items: action.items };
+    case FAIL:
+      return { ...state, failed: true, isLoading: action.isLoading };
+    default:
+      return { ...state };
+  }
+}
+
+export const loading = () => ({ type: LOADING, isLoading: true });
+
+export const success = items => ({ type: SUCCESS, isLoading: false, items });
+
+export const fail = () => ({ type: FAIL, isLoading: false });
+
+export const fetchPhotos = () => async dispatch => {
+  dispatch(loading());
+  try {
+    const photos = await getPhotos();
+    const normalizedData = normalize(photos, photoList);
+    dispatch(success(normalizedData.entities.photos));
+  } catch (err) {
+    dispatch(fail());
+  }
+};
