bc5b004eb4383e419419ce53859d52cf4206f3e0
toumas <tuomasukk@gmail.com>
2018-04-29 22:54:21 +0300

    Added home routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+import { Route } from 'react-router';
+import { ConnectedRouter } from 'react-router-redux';
+import { store, history } from './store';
 import Home from './Home';
 
 const App = () => (
   <Provider store={store}>
-    <Home />
+    <ConnectedRouter history={history}>
+      <div>
+        <Route exact path="/" component={Home} />
+      </div>
+    </ConnectedRouter>
   </Provider>
 );
 
--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
 import reducers from './reducers';
 
 // eslint-disable-next-line no-underscore-dangle, no-undef
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+export const history = createHistory();
+const routerHistoryMiddleware = routerMiddleware(history);
+
+export const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk, routerHistoryMiddleware)),
+);
