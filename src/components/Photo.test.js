e39bf39b29eedb5ddde9e815b35d6235920c5e7b
toumas <tuomasukk@gmail.com>
2018-06-02 20:22:05 +0300

    Photo tests

---
+++ src/components/Photo.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Photo from './Photo';
+
+/* eslint-disable no-undef */
+
+function getProps() {
+  return {
+    isLoading: true,
+    failed: false,
+    photo: {
+      albumId: 1,
+      id: 1,
+      title: 'accusamus beatae ad facilis cum similique qui sunt',
+      url: 'http://placehold.it/600/92c952',
+      thumbnailUrl: 'http://placehold.it/150/92c952',
+    },
+    retry: () => {},
+  };
+}
+
+describe('Photo component', () => {
+  it('should render loading text when isLoading prop equals true', () => {
+    const wrapper = shallow(<Photo {...getProps()} />);
+    expect(wrapper.text()).toEqual('Loading...');
+  });
+
+  it('should render retry component when failed prop equals true', () => {
+    const wrapper = shallow(<Photo {...getProps()} />);
+    wrapper.setProps({ failed: true, isLoading: false });
+    expect(wrapper.find('Retry')).toHaveLength(1);
+  });
+
+  it('should render photo when props isLoading and failed equal false', () => {
+    const wrapper = shallow(<Photo {...getProps()} />);
+    wrapper.setProps({ isLoading: false, failed: false });
+    expect(wrapper.find('img')).toHaveLength(1);
+  });
+});
