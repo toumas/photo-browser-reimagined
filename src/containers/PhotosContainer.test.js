32dd446de5517994bb4bc667640c0cbd1b8a1828
toumas <tuomasukk@gmail.com>
2018-06-02 14:07:18 +0300

    Added navigation/getPath test

--- src/containers/PhotosContainer.test.js
+++ src/containers/PhotosContainer.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PhotosContainer } from './PhotosContainer';
+import { PhotosContainer, getPath } from './PhotosContainer';
 
 /* eslint-disable no-undef */
 
@@ -7,10 +7,25 @@ function getProps() {
   return {
     failed: false,
     fetchPhotos: () => {},
-    getPath: () => {},
+    getPath,
     isLoading: false,
     match: { isExact: true, path: '/', url: '/', params: {} },
-    photos: [],
+    photos: [
+      {
+        albumId: 1,
+        id: 1,
+        title: 'accusamus beatae ad facilis cum similique qui sunt',
+        url: 'http://placehold.it/600/92c952',
+        thumbnailUrl: 'http://placehold.it/150/92c952',
+      },
+      {
+        albumId: 1,
+        id: 2,
+        title: 'reprehenderit est deserunt velit ipsam',
+        url: 'http://placehold.it/600/771796',
+        thumbnailUrl: 'http://placehold.it/150/771796',
+      },
+    ],
   };
 }
 
@@ -53,5 +68,39 @@ describe('PhotosContainer component', () => {
       },
     });
     expect(wrapper.state().options.albumId).toEqual('1');
+  });
+
+  it('should return proper path for navigation', () => {
+    const wrapper = mount(
+      <PhotosContainer {...getProps()}>{() => null}</PhotosContainer>,
+    );
+    expect(
+      wrapper
+        .props()
+        .getPath(
+          wrapper.props().match.url,
+          wrapper.state().options.page,
+          wrapper.props().photos[0].id,
+        ),
+    ).toEqual('/page/1/photo/1');
+    wrapper.setProps({
+      match: {
+        path: '/page/:page',
+        url: '/page/1',
+        isExact: false,
+        params: {
+          page: '1',
+        },
+      },
+    });
+    expect(
+      wrapper
+        .props()
+        .getPath(
+          wrapper.props().match.url,
+          wrapper.state().options.page,
+          wrapper.props().photos[1].id,
+        ),
+    ).toEqual('/page/1/photo/2');
   });
 });
