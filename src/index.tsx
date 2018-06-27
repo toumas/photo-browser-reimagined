ead1ee6379c766021962c3218cd6d4813c2d2de1
toumas <tuomasukk@gmail.com>
2018-06-27 21:10:13 +0300

    Basic TS setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import 'babel-polyfill';
-import React from 'react';
+import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
