c454d4a2448f89d79a2a5568a93a66e6f3b991ae
toumas <tuomasukk@gmail.com>
2018-06-29 18:38:00 +0300

    TSLint

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import 'babel-polyfill';
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import Screens from './Screens';
 import { store } from './store';
-import Screens from './screens';
 
 const App = () => (
   <Provider store={store}>
