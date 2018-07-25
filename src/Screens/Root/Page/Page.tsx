6e9c943c48337a9387ba190e73131ae9092055ad
toumas <tuomasukk@gmail.com>
2018-07-26 01:05:07 +0300

    TypeScript conversion

--- src/Screens/Root/Page/Page.tsx
+++ src/Screens/Root/Page/Page.tsx
@@ -1,7 +1,7 @@
 import React, { SFC } from 'react';
 import { match as IMatch } from 'react-router';
 
-import ScreensRoot from '../';
+import ScreensRoot from '..';
 
 interface Props {
   match: IMatch<{}>;
