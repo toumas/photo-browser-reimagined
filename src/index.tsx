77d1b48113de3fbfdd1edfd331aafccc3456ea82
toumas <tuomasukk@gmail.com>
2018-08-11 16:48:14 +0300

    Added sidebar component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import 'babel-polyfill';
+// tslint:disable-next-line
+import 'semantic-ui-css/semantic.min.css';
+
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+
 import Screens from './Screens';
 import { store } from './store';
 
