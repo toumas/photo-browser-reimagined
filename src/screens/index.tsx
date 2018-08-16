7ba3190195a64bdf182baaf15967c62bf973a38b
toumas <tuomasukk@gmail.com>
2018-08-16 22:01:00 +0300

    Added grid for photos

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
+import { Container } from 'semantic-ui-react';
 
 import Sidebar from '../components/UI/Sidebar';
 import { history } from '../store';
@@ -15,21 +16,23 @@ const Screens = (props) => (
   <ConnectedRouter history={history}>
     <React.Fragment>
       <Sidebar />
-      <Switch>
-        <Route exact={true} path="/" component={ScreensRoot} />
-        <Route path="/page/:page" component={ScreensRootPage} />
-        <Route exact={true} path="/photo/:id" component={ScreensPhoto} />
-        <Route exact={true} path="/albums" component={ScreensAlbums} />
-        <Route
-          exact={true}
-          path="/albums/page/:page"
-          component={ScreensAlbumsPage}
-        />
-        <Route
-          path="/albums/:albumId/page/:page"
-          component={ScreensAlbumsAlbumPage}
-        />
-      </Switch>
+      <Container>
+        <Switch>
+          <Route exact={true} path="/" component={ScreensRoot} />
+          <Route path="/page/:page" component={ScreensRootPage} />
+          <Route exact={true} path="/photo/:id" component={ScreensPhoto} />
+          <Route exact={true} path="/albums" component={ScreensAlbums} />
+          <Route
+            exact={true}
+            path="/albums/page/:page"
+            component={ScreensAlbumsPage}
+          />
+          <Route
+            path="/albums/:albumId/page/:page"
+            component={ScreensAlbumsAlbumPage}
+          />
+        </Switch>
+      </Container>
     </React.Fragment>
   </ConnectedRouter>
 );
